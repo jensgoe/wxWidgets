b256ef44227d9f4cd48ad306ef0a3d05678c8815
jensgoe <mail@jensgoepfert.de>
2018-03-24 00:10:20 +0100

    fixed infinite loops due to overflows of unsigned variables

--- src/generic/datavgen.cpp
+++ src/generic/datavgen.cpp
@@ -823,7 +823,7 @@ public:
 
     int GetLineStart( unsigned int row ) const;  // row * m_lineHeight in fixed mode
     int GetLineHeight( unsigned int row ) const; // m_lineHeight in fixed mode
-    int GetLineAt( unsigned int y ) const;       // y / m_lineHeight in fixed mode
+    int GetLineAt( int y ) const;                // y / m_lineHeight in fixed mode
 
     void SetRowHeight( int lineHeight ) { m_lineHeight = lineHeight; }
     int GetRowHeight() const { return m_lineHeight; }
@@ -3304,6 +3304,8 @@ void wxDataViewMainWindow::SendSelectionChangedEvent( const wxDataViewItem& item
 
 void wxDataViewMainWindow::RefreshRow( unsigned int row )
 {
+    if (row == ((unsigned)-1))
+        return;
     wxRect rect( 0, GetLineStart( row ), GetEndOfLastCol(), GetLineHeight( row ) );
     m_owner->CalcScrolledPosition( rect.x, rect.y, &rect.x, &rect.y );
 
@@ -3422,8 +3424,10 @@ int wxDataViewMainWindow::GetLineStart( unsigned int row ) const
     }
 }
 
-int wxDataViewMainWindow::GetLineAt( unsigned int y ) const
+int wxDataViewMainWindow::GetLineAt( int y ) const
 {
+    if (y < 0) return -1;
+
     const wxDataViewModel *model = GetModel();
 
     // check for the easy case first
